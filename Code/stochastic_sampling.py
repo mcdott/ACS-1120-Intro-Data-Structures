535d4167e05b4cdbee7b53e2a8c9595e61e47639
mcdott <111994803+mcdott@users.noreply.github.com>
2023-05-28 10:56:14 -0400

    Replaced dictionary with list of tuples in weighted random word function

--- Code/stochastic_sampling.py
+++ Code/stochastic_sampling.py
@@ -1,3 +1,38 @@
+# Generate a single, weighted (based on word frequency) random word 
+# from a dictionary (created from a text file)
+# import random
+# import sys
+
+# def weighted_random_word(filename):
+#     # Open the file
+#     with open(filename, 'r') as f:
+#         # Read the file and split lines
+#         lines = f.readlines()
+
+#     # Convert lines to a dictionary
+#     histogram = {line.split()[0]: int(line.split()[1]) for line in lines}
+
+#     # Calculate the sum of all frequencies
+#     total = sum(histogram.values())
+
+#     # Generate a random number between 0 and the total
+#     rand = random.randint(0, total - 1)
+
+#     # Subtract each word's frequency from the random number until we reach 0
+#     # (words with higher frequencies will fill a larger range in this interval 
+#     # and are therefore more likely to be chosen)
+#     for word, freq in histogram.items():
+#         rand -= freq
+#         if rand < 0:
+#             return word
+
+# if __name__ == "__main__":
+#     print(weighted_random_word(sys.argv[1]))
+
+# >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+# Generate a single, weighted (based on word frequency) random word 
+# from a list of tuples (created from a text file)
 import random
 import sys
 
@@ -7,19 +42,19 @@ def weighted_random_word(filename):
         # Read the file and split lines
         lines = f.readlines()
 
-    # Convert lines to a dictionary
-    histogram = {line.split()[0]: int(line.split()[1]) for line in lines}
+    # Convert lines to a list of tuples (word, frequency)
+    histogram = [(line.split()[0], int(line.split()[1])) for line in lines]
 
     # Calculate the sum of all frequencies
-    total = sum(histogram.values())
+    total = sum(freq for _, freq in histogram)
 
     # Generate a random number between 0 and the total
     rand = random.randint(0, total - 1)
 
     # Subtract each word's frequency from the random number until we reach 0
-    # (words with higher frequencies will fill a larger range in this interval 
-    # and are more likely to be chosen)
-    for word, freq in histogram.items():
+    # (words with higher frequencies will fill a larger range in this interval
+    # and are therefore more likely to be chosen)
+    for word, freq in histogram:
         rand -= freq
         if rand < 0:
             return word
@@ -28,7 +63,7 @@ if __name__ == "__main__":
     print(weighted_random_word(sys.argv[1]))
 
 
-
+# >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 
 # Generate a single random word from a text file
 # import random
