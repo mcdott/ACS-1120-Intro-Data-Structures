e5bb538a58781baf352b1b422b1620c9dafd4ede
mcdott <111994803+mcdott@users.noreply.github.com>
2023-05-28 10:36:33 -0400

    Added weighted random word function that takes word frequency into account

--- Code/stochastic_sampling.py
+++ Code/stochastic_sampling.py
@@ -1,21 +1,54 @@
 import random
 import sys
 
-def random_word(filename):
+def weighted_random_word(filename):
     # Open the file
     with open(filename, 'r') as f:
         # Read the file and split lines
         lines = f.readlines()
 
-    # Get the words only (ignoring the counts)
-    words = [line.split()[0] for line in lines]
+    # Convert lines to a dictionary
+    histogram = {line.split()[0]: int(line.split()[1]) for line in lines}
 
-    # Generate a random index based on the length of words
-    random_index = random.randint(0, len(words) - 1)
+    # Calculate the sum of all frequencies
+    total = sum(histogram.values())
 
-    # Return a random word
-    return words[random_index]
+    # Generate a random number between 0 and the total
+    rand = random.randint(0, total - 1)
+
+    # Subtract each word's frequency from the random number until we reach 0
+    # (words with higher frequencies will fill a larger range in this interval 
+    # and are more likely to be chosen)
+    for word, freq in histogram.items():
+        rand -= freq
+        if rand < 0:
+            return word
 
 if __name__ == "__main__":
-    # pass in the path to the text file as an argument
-    print(random_word(sys.argv[1]))
+    print(weighted_random_word(sys.argv[1]))
+
+
+
+
+# Generate a single random word from a text file
+# import random
+# import sys
+
+# def random_word(filename):
+#     # Open the file
+#     with open(filename, 'r') as f:
+#         # Read the file and split lines
+#         lines = f.readlines()
+
+#     # Get the words only (ignoring the counts)
+#     words = [line.split()[0] for line in lines]
+
+#     # Generate a random index based on the length of words
+#     random_index = random.randint(0, len(words) - 1)
+
+#     # Return a random word
+#     return words[random_index]
+
+# if __name__ == "__main__":
+#     # pass in the path to the text file as an argument
+#     print(random_word(sys.argv[1]))
--- Code/word_frequency.py
+++ Code/word_frequency.py
@@ -1,127 +1,57 @@
-# import string
-
-# def histogram(source_text):
-#     """
-#     This function takes a string as input, and processes the string to return a histogram as a dictionary.
-#     """
-#     # Remove punctuation and convert to lowercase
-#     translator = str.maketrans('', '', string.punctuation)
-#     source_text = source_text.translate(translator).lower()
-
-#     # Create a histogram
-#     word_counts = {}
-#     for word in source_text.split():
-#         if word in word_counts:
-#             word_counts[word] += 1
-#         else:
-#             word_counts[word] = 1
-
-#     return word_counts
-
-# def write_histogram_to_file(hist, filename):
-#     """
-#     This function takes a histogram and a filename, and writes the histogram to the file. Each line contains
-#     a word and its count, separated by a space.
-#     """
-#     with open(filename, 'w') as f:
-#         for word, count in hist.items():
-#             f.write(f'{word} {count}\n')
-
-
-# def read_histogram_from_file(filename):
-#     """
-#     This function takes a filename and returns a histogram (dictionary of word counts) based on the file.
-#     """
-#     hist = {}
-#     with open(filename, 'r') as f:
-#         for line in f:
-#             word, count = line.split()
-#             hist[word] = int(count)
-#     return hist
-
-# def unique_words(hist):
-#     """
-#     This function takes a histogram (dictionary of word counts) and returns the number of unique words.
-#     """
-#     return len(hist)
-
-
-# def frequency(word, hist):
-#     """
-#     This function takes a word and a histogram (dictionary of word counts), and returns the frequency of that word.
-#     """
-#     return hist.get(word, 0)
-
-
-# def main():
-#     # Read the text file
-#     with open('data/corpus.txt', 'r') as f:
-#         source_text = f.read()
-
-#     # Calculate the histogram
-#     hist = histogram(source_text)
-
-#     # Write the histogram to a file
-#     write_histogram_to_file(hist, 'data/histogram.txt')
-
-#     # Read the histogram from the file
-#     hist_from_file = read_histogram_from_file('data/histogram.txt')
-
-#     print("Number of unique words:", unique_words(hist))
-#     print("Frequency of 'bottle':", frequency('bottle', hist))
-
-#     # Compute some statistics
-#     values = list(hist.values())
-#     mean = sum(values) / len(values)
-#     median = sorted(values)[len(values) // 2]
-#     mode = max(set(values), key=values.count)
-    
-#     print("Mean word frequency:", mean)
-#     print("Median word frequency:", median)
-#     print("Mode word frequency:", mode)
-
-
-# if __name__ == "__main__":
-#     main()
-
-
-# Or, using a list of lists instead of a dictionary:
 import string
 
 def histogram(source_text):
     """
-    This function takes a string as input, and processes the string to return a histogram as a list of lists.
+    This function takes a string as input, and processes the string to return a histogram as a dictionary.
     """
     # Remove punctuation and convert to lowercase
     translator = str.maketrans('', '', string.punctuation)
     source_text = source_text.translate(translator).lower()
 
     # Create a histogram
-    word_counts = []
+    word_counts = {}
     for word in source_text.split():
-        for sub_list in word_counts:
-            if word == sub_list[0]:
-                sub_list[1] += 1
-                break
+        if word in word_counts:
+            word_counts[word] += 1
         else:
-            word_counts.append([word, 1])
+            word_counts[word] = 1
 
     return word_counts
 
+def write_histogram_to_file(hist, filename):
+    """
+    This function takes a histogram and a filename, and writes the histogram to the file. Each line contains
+    a word and its count, separated by a space.
+    """
+    with open(filename, 'w') as f:
+        for word, count in hist.items():
+            f.write(f'{word} {count}\n')
+
+
+def read_histogram_from_file(filename):
+    """
+    This function takes a filename and returns a histogram (dictionary of word counts) based on the file.
+    """
+    hist = {}
+    with open(filename, 'r') as f:
+        for line in f:
+            word, count = line.split()
+            hist[word] = int(count)
+    return hist
+
 def unique_words(hist):
     """
-    This function takes a histogram (list of word counts) and returns the number of unique words.
+    This function takes a histogram (dictionary of word counts) and returns the number of unique words.
     """
     return len(hist)
 
+
 def frequency(word, hist):
     """
-    This function takes a word and a histogram (list of word counts), and returns the frequency of that word.
+    This function takes a word and a histogram (dictionary of word counts), and returns the frequency of that word.
     """
-    for sub_list in hist:
-        if word == sub_list[0]:
-            return sub_list[1]
-    return 0
+    return hist.get(word, 0)
+
 
 def main():
     # Read the text file
@@ -131,18 +61,88 @@ def main():
     # Calculate the histogram
     hist = histogram(source_text)
 
+    # Write the histogram to a file
+    write_histogram_to_file(hist, 'data/histogram.txt')
+
+    # Read the histogram from the file
+    hist_from_file = read_histogram_from_file('data/histogram.txt')
+
     print("Number of unique words:", unique_words(hist))
     print("Frequency of 'bottle':", frequency('bottle', hist))
 
     # Compute some statistics
-    frequencies = [sub_list[1] for sub_list in hist]
-    mean = sum(frequencies) / len(frequencies)
-    median = sorted(frequencies)[len(frequencies) // 2]
-    mode = max(set(frequencies), key=frequencies.count)
+    values = list(hist.values())
+    mean = sum(values) / len(values)
+    median = sorted(values)[len(values) // 2]
+    mode = max(set(values), key=values.count)
     
     print("Mean word frequency:", mean)
     print("Median word frequency:", median)
     print("Mode word frequency:", mode)
 
+
 if __name__ == "__main__":
     main()
+
+
+# Or, using a list of lists instead of a dictionary:
+# import string
+
+# def histogram(source_text):
+#     """
+#     This function takes a string as input, and processes the string to return a histogram as a list of lists.
+#     """
+#     # Remove punctuation and convert to lowercase
+#     translator = str.maketrans('', '', string.punctuation)
+#     source_text = source_text.translate(translator).lower()
+
+#     # Create a histogram
+#     word_counts = []
+#     for word in source_text.split():
+#         for sub_list in word_counts:
+#             if word == sub_list[0]:
+#                 sub_list[1] += 1
+#                 break
+#         else:
+#             word_counts.append([word, 1])
+
+#     return word_counts
+
+# def unique_words(hist):
+#     """
+#     This function takes a histogram (list of word counts) and returns the number of unique words.
+#     """
+#     return len(hist)
+
+# def frequency(word, hist):
+#     """
+#     This function takes a word and a histogram (list of word counts), and returns the frequency of that word.
+#     """
+#     for sub_list in hist:
+#         if word == sub_list[0]:
+#             return sub_list[1]
+#     return 0
+
+# def main():
+#     # Read the text file
+#     with open('data/corpus.txt', 'r') as f:
+#         source_text = f.read()
+
+#     # Calculate the histogram
+#     hist = histogram(source_text)
+
+#     print("Number of unique words:", unique_words(hist))
+#     print("Frequency of 'bottle':", frequency('bottle', hist))
+
+#     # Compute some statistics
+#     frequencies = [sub_list[1] for sub_list in hist]
+#     mean = sum(frequencies) / len(frequencies)
+#     median = sorted(frequencies)[len(frequencies) // 2]
+#     mode = max(set(frequencies), key=frequencies.count)
+    
+#     print("Mean word frequency:", mean)
+#     print("Median word frequency:", median)
+#     print("Mode word frequency:", mode)
+
+# if __name__ == "__main__":
+#     main()
