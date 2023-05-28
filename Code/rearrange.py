61fb881a97416d6abfb7909d564a8a7b9f364534
mcdott <111994803+mcdott@users.noreply.github.com>
2023-05-28 08:57:30 -0400

    Added histogram as a list of lists

--- Code/rearrange.py
+++ Code/rearrange.py
@@ -9,3 +9,11 @@ if __name__ == '__main__':
     words = sys.argv[1:]
     rearranged_words = rearrange(words)
     print(rearranged_words)
+
+# Or, without using the `shuffle` method:
+# def rearrange(words):
+#     rearranged_words = []
+#     while len(words) > 0:
+#         rand_index = random.randint(0, len(words) - 1)
+#         rearranged_words.append(words.pop(rand_index))
+#     return ' '.join(rearranged_words)
