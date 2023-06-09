f0a12e89a120bf6f217f536e588f89f454ccc95b
mcdott <111994803+mcdott@users.noreply.github.com>
2023-06-09 16:36:55 -0400

    Adds find, delete, and replace methods

--- Code/linkedlist.py
+++ Code/linkedlist.py
@@ -86,18 +86,51 @@ class LinkedList:
 
     def find(self, matcher):
         """Return an item from this linked list if it is present.
-        TODO: Best case running time: O(???) Why and under what conditions?
-        TODO: Worst case running time: O(???) Why and under what conditions?"""
-        # TODO: Loop through all nodes to find item, if present return True otherwise False
+        Best case running time: O(1) When the item is first in the list.
+        Worst case running time: O(n) When the item is last in the list 
+        or not in the list."""
+        node = self.head  # O(1) time to assign new variable
+        while node is not None:  # Up to n iterations
+            if matcher == node.data:  # O(1) time to check equality
+                return True  # O(1) time to return value
+            node = node.next  # O(1) time to assign new variable
+        return False  # O(1) time to return value
 
     def delete(self, item):
         """Delete the given item from this linked list, or raise ValueError.
-        TODO: Best case running time: O(???) Why and under what conditions?
-        TODO: Worst case running time: O(???) Why and under what conditions?"""
-        # TODO: Loop through all nodes to find one whose data matches given item
-        # TODO: Update previous node to skip around node with matching data
-        # TODO: Otherwise raise error to tell user that delete has failed
-        # Hint: raise ValueError('Item not found: {}'.format(item))
+        Best case running time: O(1) When the item is first in the list.
+        Worst case running time: O(n) When the item is last in the list."""
+        node = self.head  # O(1) time to assign new variable
+        previous_node = None  # O(1) time to assign new variable
+        while node is not None:  # Up to n iterations
+            if item == node.data:  # O(1) time to check equality
+                if node is not self.head and node is not self.tail:  # O(1) time to check equality
+                    previous_node.next = node.next  # O(1) time to assign next of previous node
+                elif node is self.head:  # O(1) time to check equality
+                    self.head = node.next  # O(1) time to assign new head
+                    if node is self.tail:  # O(1) time to check equality
+                        self.tail = None  # O(1) time to assign new tail
+                elif node is self.tail:  # O(1) time to check equality
+                    self.tail = previous_node  # O(1) time to assign new tail
+                    if previous_node:  # O(1) time to check if previous node is not None
+                        previous_node.next = None  # O(1) time to assign next of previous node
+                return  # O(1) time to return
+            previous_node = node  # O(1) time to assign new variable
+            node = node.next  # O(1) time to assign new variable
+        raise ValueError('Item not found: {}'.format(item))  # O(1) time to raise exception
+    
+    def replace(self, old_item, new_item):
+        """Replace the old item with the new item in the linked list, 
+        or raise ValueError if the old item is not found. 
+        Running time: O(n) because in the worst case we need to go through all nodes in the list."""
+        node = self.head  # O(1) time to assign new variable
+        while node is not None:  # Up to n iterations
+            if old_item == node.data:  # O(1) time to check equality
+                node.data = new_item  # O(1) time to replace data
+                return  # O(1) time to return
+            node = node.next  # O(1) time to assign new variable
+        raise ValueError('Item not found: {}'.format(old_item))  # O(1) time to raise exception
+
 
 
 def test_linked_list():
@@ -114,7 +147,7 @@ def test_linked_list():
     print('length: {}'.format(ll.length()))
 
     # Enable this after implementing delete method
-    delete_implemented = False
+    delete_implemented = True
     if delete_implemented:
         print('\nTesting delete:')
         for item in ['B', 'C', 'A']:
