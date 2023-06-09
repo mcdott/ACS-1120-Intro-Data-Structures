6b2a0a6a6fcb28cc64063987839e04a910f38ccf
mcdott <111994803+mcdott@users.noreply.github.com>
2023-06-08 22:32:22 -0400

    Adds add_count and frequency methods

--- Code/dictogram.py
+++ Code/dictogram.py
@@ -20,11 +20,17 @@ class Dictogram(dict):
 
     def add_count(self, word, count=1):
         """Increase frequency count of given word by given count amount."""
-        # TODO: Increase word frequency by count
+        if word not in self:
+            self.types += 1
+        self[word] = self.get(word, 0) + count
+        self.tokens += count
 
     def frequency(self, word):
         """Return frequency count of given word, or 0 if word is not found."""
-        # TODO: Retrieve word frequency count
+        if word in self:
+            return self[word]
+        else:
+            return 0
 
     def sample(self):
         """Return a word from this histogram, randomly sampled by weighting
