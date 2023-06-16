206006dc2956a8998aff33746745d2daa93a9c1e
mcdott <111994803+mcdott@users.noreply.github.com>
2023-06-16 12:37:11 -0400

    Encapsulates methods into TextTokenizer class

--- Code/tokens.py
+++ Code/tokens.py
@@ -1,18 +1,29 @@
 import re
+import sys
 
-def tokenize(text):
-    tokens = split_on_whitespace(text)
-    return tokens
+class TextTokenizer:
+    def __init__(self, text=""):
+        self.text = text
 
-def split_on_whitespace(text):
-    return re.split('\s+', text)
+    def tokenize(self):
+        no_punc_text = self.remove_punctuation()
+        tokens = self.split_on_whitespace(no_punc_text)
+        return tokens
+
+    def remove_punctuation(self):
+        no_punc_text = re.sub('[,.()]', '', self.text)
+        no_punc_text = re.sub('--', ' ', no_punc_text)
+        return no_punc_text
+
+    def split_on_whitespace(self, text):
+        return re.split('\s+', text)
 
 if __name__ == '__main__':
-    import sys
     if len(sys.argv) > 1:
         filename = sys.argv[1]
         source = open(filename).read()
-        tokens = tokenize(source)
+        tokenizer = TextTokenizer(source)
+        tokens = tokenizer.tokenize()
         print(tokens)
     else:
         print('No source text filename given as argument')
