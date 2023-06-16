b98c979857d02090cabb5d3bbeeacd90217cff10
mcdott <111994803+mcdott@users.noreply.github.com>
2023-06-16 12:13:03 -0400

    Sets up basic tokenizer to split text from a file into tokens

--- Code/tokens.py
+++ Code/tokens.py
@@ -1,1 +1,18 @@
-# Module for creating lists of tokens from a text
+import re
+
+def tokenize(text):
+    tokens = split_on_whitespace(text)
+    return tokens
+
+def split_on_whitespace(text):
+    return re.split('\s+', text)
+
+if __name__ == '__main__':
+    import sys
+    if len(sys.argv) > 1:
+        filename = sys.argv[1]
+        source = open(filename).read()
+        tokens = tokenize(source)
+        print(tokens)
+    else:
+        print('No source text filename given as argument')
