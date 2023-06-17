c370c1dc9bfd6e71ed0a9e2d69c5b2c81df6212a
mcdott <111994803+mcdott@users.noreply.github.com>
2023-06-17 11:07:32 -0400

    Jabbercises character names in 'Alice in Wonderland' corpus

--- app.py
+++ app.py
@@ -9,16 +9,16 @@ app = Flask(__name__)
 # Prepare the corpus and initialize markov chain.
 # This code will run only once, when the server starts.
 corpus_path = 'corpus.txt'
-cleaned_corpus_path = 'cleaned_corpus.txt'
+preparred_corpus_path = 'preparred_corpus.txt'
 
 # Clean the corpus
 parser = TextParser(corpus_path)
-parser.save_cleaned_text(cleaned_corpus_path)
+parser.save_preparred_text(preparred_corpus_path)
 
 # Tokenize the cleaned corpus
-with open(cleaned_corpus_path, 'r') as file:
-    cleaned_corpus = file.read()
-tokenizer = TextTokenizer(cleaned_corpus)
+with open(preparred_corpus_path, 'r') as file:
+    preparred_corpus = file.read()
+tokenizer = TextTokenizer(preparred_corpus)
 tokenized_corpus = tokenizer.tokenize()
 
 # Generate sentences
--- cleanup.py
+++ cleanup.py
@@ -2,6 +2,26 @@ class TextParser:
 
     def __init__(self, corpus_path):
         self.corpus_path = corpus_path
+        self.character_names = {
+            'Alice': 'Riddlishy',
+            'White Rabbit': 'Hareflustious',
+            'Queen of Hearts': 'Queebeheaderous',
+            'King of Hearts': 'King Mildemane',
+            'Cheshire Cat': 'Grinsvanishire',
+            'Mad Hatter': 'Teatimzy Lunatico',
+            'March Hare': 'Springbound Nuttersly',
+            'Dormouse': 'Dozesqueakle',
+            'Caterpillar': 'Puffosophor',
+            'Duchess': 'Duchiwrath',
+            'Knave of Hearts': 'Knave Tartsnatcher',
+            'Tweedledee': 'Dumbleriddle',
+            'Tweedledum': 'Dumblerumble',
+            'Mock Turtle': 'Turtly Facado',
+            'Gryphon': 'Eaglionix',
+            'Bill the Lizard': 'Lizascaler Billius',
+            'Dodo': 'Dodgsonic',
+            'Mouse': 'Rodentiquake',
+        }
 
     def read_file(self):
         with open(self.corpus_path, 'r') as file:
@@ -21,13 +41,19 @@ class TextParser:
         corpus = corpus.replace('’', "'")
         return corpus
     
-    def clean_text(self):
+    def replace_character_names(self, corpus):
+        for original_name, jabbercised_name in self.character_names.items():
+            corpus = corpus.replace(original_name, jabbercised_name)
+        return corpus
+    
+    def preparred_text(self):
         corpus = self.read_file()
         corpus = self.remove_unwanted_punctuation(corpus)
         corpus = self.standardize_quotes(corpus)
+        corpus = self.replace_character_names(corpus)
         return corpus
     
-    def save_cleaned_text(self, output_filename):
-        cleaned_corpus = self.clean_text()
+    def save_preparred_text(self, output_filename):
+        preparred_corpus = self.preparred_text()
         with open(output_filename, 'w') as file:
-            file.write(cleaned_corpus)
+            file.write(preparred_corpus)
