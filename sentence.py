a0ff9cf26c6087a02f5bfe95eccd94bfb868bda4
mcdott <111994803+mcdott@users.noreply.github.com>
2023-06-17 14:55:43 -0400

    Updates to 2nd order markov chain

--- sentence.py
+++ sentence.py
@@ -10,23 +10,24 @@ class MarkovSentenceGenerator:
         words = self.tokenized_corpus
         markov_dict = {}
 
-        for i in range(len(words) - 1):
-            if words[i] in markov_dict:
-                markov_dict[words[i]].append(words[i + 1])
+        for i in range(len(words) - 2):
+            pair = (words[i], words[i + 1])
+            if pair in markov_dict:
+                markov_dict[pair].append(words[i + 2])
             else:
-                markov_dict[words[i]] = [words[i + 1]]
+                markov_dict[pair] = [words[i + 2]]
 
         return markov_dict
 
     def generate_sentence(self, num_words):
-        current_word = random.choice(list(self.markov_dict.keys()))
-        sentence = current_word
+        current_pair = random.choice(list(self.markov_dict.keys()))
+        sentence = current_pair[0] + ' ' + current_pair[1]
 
-        for i in range(num_words - 1):
-            if current_word in self.markov_dict:
-                next_word = random.choice(self.markov_dict[current_word])
+        for i in range(num_words - 2):
+            if current_pair in self.markov_dict:
+                next_word = random.choice(self.markov_dict[current_pair])
                 sentence += ' ' + next_word
-                current_word = next_word
+                current_pair = (current_pair[1], next_word)
             else:
                 break
 
