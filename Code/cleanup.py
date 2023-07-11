9ca9a44ddfea2f60e4c3334ebd62706800aea7dc
mcdott <111994803+mcdott@users.noreply.github.com>
2023-07-11 15:32:21 -0400

    Adds more portmanteau substitutions to corpus

--- Code/cleanup.py
+++ Code/cleanup.py
@@ -2,6 +2,108 @@ class TextParser:
 
     def __init__(self, corpus_path):
         self.corpus_path = corpus_path
+        self.common_wonderland_words = {
+            'White Rabbit': 'Hareflustious',
+            'Queen of Hearts': 'Queebeheaderous',
+            'Queen': 'Queebeheaderous',
+            'King of Hearts': 'King Mildemane',
+            'Cheshire Cat': 'Grinsvanishire',
+            'Mad Hatter': 'Teatimzy Lunatico',
+            'Hatter': 'Teatimzy',
+            'March Hare': 'Springbound Nuttersly',
+            'Hare': 'Springbound',
+            'Dormouse': 'Dozesqueakle',
+            'Caterpillar': 'Puffosophor',
+            'Duchess': 'Duchiwrath',
+            'Knave of Hearts': 'Knave Tartsnatcher',
+            'Tweedledee': 'Dumbleriddle',
+            'Tweedledum': 'Dumblerumble',
+            'Mock Turtle': 'Turtly Facado',
+            'Gryphon': 'Eaglionix',
+            'Bill the Lizard': 'Lizascaler Billius',
+            'Dodo': 'Dodgsonic',
+            'Mouse': 'Rodentiquake',
+            'Wonderland': 'Jabberland',
+            'Rabbit Hole': 'Raburrow',
+            'The Pool of Tears': 'Weepuddle',
+            'The Caucus Race': 'Rundebate',
+            'The Duchess\'s Kitchen': 'Cookduchery',
+            'Mad Tea-Party': 'Lunachai',
+            'Queen\'s Croquet Ground': 'Quequet Field',
+            'The Mock Turtle\'s Story': 'Tale-Turtlery',
+            'The Lobster Quadrille': 'Lobstrance',
+            'The Knave of Hearts\' Trial': 'Justiknavery',
+            'The White Rabbit\'s House': 'Bunnicottage',
+            'Eat Me Cake': 'Ingestibake',
+            'Drink Me Potion': 'Sipsolution',
+            'Cheshire Cat\'s Grin': 'Smileshire',
+            'Tarts of the Queen': 'Queentreats',
+            'Flamingo': 'Flamingleap',
+            'Hedgehog': 'Spinecurl',
+            'Puppy': 'Puppounce',
+            'Fish': 'Fisplash',
+            'Frog': 'Leapcroak',
+            'Pig': 'Snoutgrunt',
+            'Baby': 'Babblooth',
+            'Pigeon': 'Cooclaw',
+            'Lory': 'Lorflutter',
+            'Duck': 'Quackfloat',
+            'Crab': 'Crabscuttle',
+            'Owl': 'Hootstare',
+            'Panther': 'Prowlur',
+            'Eaglet': 'Eaglide',
+            'Rose': 'Rosprickle',
+            'Violet': 'Violethide',
+            'Lily': 'Lilglisten',
+            'Daisy': 'Daisglow',
+            'slimy': 'slithy',
+            'lithe': 'slithy',
+            'gyrate': 'gyre',
+            'dire': 'gyre',
+            'gimlet': 'gimble',
+            'nimble': 'gimble',
+            'miserable': 'mimsy',
+            'flimsy': 'mimsy',
+            'bore': 'borogove',
+            'grove': 'borogove',
+            'from home': 'mome',
+            'rath': 'rath',
+            'wraith': 'rath',
+            'outgribe': 'outgrabe',
+            'grave': 'outgrabe',
+            'jubilant': 'jubjub',
+            'stubborn': 'jubjub',
+            'furious': 'frumious',
+            'fuming': 'frumious',
+            'gruffish': 'uffish',
+            'huffish': 'uffish',
+            'hurrah': 'callooh',
+            'hallelujah': 'callooh',
+            'oh': 'callay',
+            'yay': 'callay',
+            'time': 'timension',
+            'eyes': 'eyescapes',
+            'door': 'doorniverse',
+            'day': 'daydreamsicle',
+            'white': 'whilight',
+            'large': 'largeful',
+            'small': 'smallsome',
+            'round': 'roundorama',
+            'book': 'booksplore',
+            'arm': 'armstrong',
+            'feet': 'feetstumps',
+            'garden': 'gardenchant',
+            'heart': 'heartmosphere',
+            'game': 'gamestacy',
+            'see': 'seeclipse',
+            'never': 'neverdawn',
+            'thought': 'thoughtrain',
+            'looked': 'looklusioned',
+            'must': 'mustmuse',
+            'head': 'headventure',
+            'voice': 'voiceloom',
+            'began': 'begantic',
+        }
 
     def read_file(self):
         with open(self.corpus_path, 'r') as file:
@@ -21,17 +123,19 @@ class TextParser:
         corpus = corpus.replace('’', "'")
         return corpus
     
-    def clean_text(self):
+    def jabberize_common_wonderland_words(self, corpus):
+        for wonderland_word, jabberized_word in self.common_wonderland_words.items():
+            corpus = corpus.replace(wonderland_word, jabberized_word)
+        return corpus
+    
+    def cleaned_jabberized_text(self):
         corpus = self.read_file()
         corpus = self.remove_unwanted_punctuation(corpus)
         corpus = self.standardize_quotes(corpus)
+        corpus = self.jabberize_common_wonderland_words(corpus)
         return corpus
     
-    def save_cleaned_text(self, output_filename):
-        cleaned_corpus = self.clean_text()
+    def save_cleaned_jabberized_text(self, output_filename):
+        jabberized_corpus = self.cleaned_jabberized_text()
         with open(output_filename, 'w') as file:
-            file.write(cleaned_corpus)
-    
-# Usage
-parser = TextParser('data/corpus.txt')
-parser.save_cleaned_text('data/cleaned_corpus.txt')
+            file.write(jabberized_corpus)
